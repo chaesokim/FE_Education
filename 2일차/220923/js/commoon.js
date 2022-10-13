4917d7ccbe6e1f4420423edfe1f1467b1ab5aa36
chaesokim <chaesofedev@gmail.com>
2022-10-13 11:40:03 +0900

    코드 업로드

---
+++ 2일차/220923/js/commoon.js
@@ -0,0 +1,2 @@
+// document.getElementById("box").innerHTML += "<h2>조작했다</h2>";
+// document.querySelector("#bby p").innerHTML = "오나왔다";
---
+++ 8일차/green_migun_start/js/common.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $("#allMenu").click(function(){
+        $(this).toggleClass('show'); // this : 나를 호출한 객체 ==> allmenu
+    })
+})
